60414e81b08de939f0ed6fab6281285eb406eb33
Taylor Baldwin <taylorbaldwin@gmail.com>
2022-08-23 21:05:53 +0000

    upload tarballs for entire project

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 echo "[$(date --rfc-3339=sec)] downloading data"
-curl $DB_URL > /app/citibike/stage.db
+curl $TAR_URL > /app/citibike.tar.gz
+
+echo "[$(date --rfc-3339=sec)] extracting data"
+tar xvf /app/citibike.tar.gz
 
 echo "[$(date --rfc-3339=sec)] starting rill"
 cd /app/citibike
