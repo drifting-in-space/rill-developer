85218224a22c6f541761c460be34f3fb8d6b62c2
Abhishek Cherath <abhi@quic.us>
2022-09-23 16:13:32 -0400

    modify for arbitrary url

--- run.sh
+++ run.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-echo "[$(date --rfc-3339=sec)] downloading data"
-curl $TAR_URL > /app/citibike.tar.gz
 
-echo "[$(date --rfc-3339=sec)] extracting data"
-tar xvf /app/citibike.tar.gz
+#download data
+mkdir /app/data
+cd /app/data || exit
+curl -LJ "$JAMSOCKET_ZIPPED_DATA" -o 'data.zip'
+unzip 'data.zip' -d .
+rm data.zip
+for file in *; do 
+    case $file in
+        *.csv|*.parquet|*.tsv) /app/rill import-source "$file";;
+        *) echo "$file";;
+    esac
+done
 
 echo "[$(date --rfc-3339=sec)] starting rill"
-cd /app/citibike
 /app/rill start
